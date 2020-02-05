01d01b197e1a5e02a3153298e685983ea0faf485
Job <jobtalle@hotmail.com>
2020-02-06 00:32:49 +0100

    Inspector is an overlay

--- js/agent/tentacles.js
+++ js/agent/tentacles.js
@@ -1,25 +1,22 @@
 const Tentacles = function(position, direction) {
     this.tentacles = [
-        new Tentacle(position, direction, 12, 8, 2),
-        new Tentacle(position, direction, 12, 8, 2)
+        new Tentacle(position, direction, 15, 8, 2)
     ];
 
     this.wiggle = 0;
 };
 
 Tentacles.prototype.update = function(timeStep, anchor, direction, velocity) {
-    this.wiggle += 4 * timeStep;
+    this.wiggle += 6 * timeStep;
 
     if (this.wiggle > Math.PI + Math.PI)
         this.wiggle -= Math.PI + Math.PI;
 
-    let wiggle = (.5 + .5 * Math.sin(this.wiggle)) * Math.PI * 0.5;
+    let wiggle = Math.sin(this.wiggle) * Math.PI * 0.5;
 
     for (const tentacle of this.tentacles) {
         tentacle.setAnchor(anchor, direction.angle() + wiggle);
         tentacle.update(timeStep, velocity);
-
-        wiggle = -wiggle;
     }
 };
 
