61991a825ada2ae31610221dcf9bf05107ec8a2f
jobtalle <jobtalle@hotmail.com>
2020-02-06 10:25:35 +0100

    Updated preview image

--- js/agent/tentacles.js
+++ js/agent/tentacles.js
@@ -1,6 +1,7 @@
 const Tentacles = function(position, direction) {
     this.tentacles = [
-        new Tentacle(position, direction, 15, 8, 2)
+        new Tentacle(position, direction, 12, 8, 2),
+        new Tentacle(position, direction, 12, 8, 2)
     ];
 
     this.wiggle = 0;
@@ -12,11 +13,13 @@ Tentacles.prototype.update = function(timeStep, anchor, direction, velocity) {
     if (this.wiggle > Math.PI + Math.PI)
         this.wiggle -= Math.PI + Math.PI;
 
-    let wiggle = Math.sin(this.wiggle) * Math.PI * 0.5;
+    let wiggle = Math.sin(this.wiggle) * Math.PI * 0.6;
 
     for (const tentacle of this.tentacles) {
         tentacle.setAnchor(anchor, direction.angle() + wiggle);
         tentacle.update(timeStep, velocity);
+
+        wiggle = -wiggle;
     }
 };
 
